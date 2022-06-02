77aa9927b21ead47c83660844e5fd56c6ba64902
Abhijeet Srivastava <59133851+abhi991@users.noreply.github.com>
2022-06-02 16:00:45 +0530

    Time: 506 ms (28.06%), Space: 136.4 MB (35.40%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -10,7 +10,7 @@ public:
         
         for(int i=k; i < arr.size() ; i++){
             ans.push_back(arr[dq.front()]);
-            while(!dq.empty() && dq.front() <= i-k) dq.pop_front(); 
+            while(!dq.empty() && dq.front() == i-k) dq.pop_front();
             while(!dq.empty() && arr[i] >= arr[dq.back()]) dq.pop_back();
             dq.push_back(i);
         }
