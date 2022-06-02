94ca671c22fe73ef50cf962a6ae87f186e77bbe8
Abhijeet Srivastava <59133851+abhi991@users.noreply.github.com>
2022-06-02 19:56:08 +0530

    Time: 15 ms (55.60%), Space: 6 MB (62.45%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     int romanToInt(string s) {
-    if (s.empty()) return 0;
-    
     int roman[24] = {};
     roman['I' - 'A'] = 1;
     roman['V' - 'A'] = 5;
@@ -11,11 +9,10 @@ public:
     roman['C' - 'A'] = 100;
     roman['D' - 'A'] = 500;
     roman['M' - 'A'] = 1000;
-    
-    auto sum = 0;
-    auto right = roman[s.front() - 'A'];
+    int sum = 0;
+    int right = roman[s.front() - 'A'];
     for (int i = 1; i < s.size(); ++i) {
-        auto curr = right;
+        int curr = right;
         right = roman[s[i] - 'A'];
         if (right > curr) 
             sum -= curr;
