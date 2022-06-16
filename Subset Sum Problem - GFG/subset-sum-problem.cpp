9f41749529a42939a9dbe8be514ca52e28d3808c
Abhijeet Srivastava <59133851+abhi991@users.noreply.github.com>
2022-06-16 16:29:03 +0530

    Added solution - LeetHub

--- Subset Sum Problem - GFG/subset-sum-problem.cpp
+++ Subset Sum Problem - GFG/subset-sum-problem.cpp
@@ -8,24 +8,37 @@ using namespace std;
 //User function template for C++
 
 class Solution{
-private:
-    int dp[101][10000];
 public:
-    Solution(){
-        memset(dp , -1 , sizeof(dp));
-    }
-    bool isSubsetSum(vector<int>a, int sum ,int i=0){
+    // Solution(){
+    //     memset(dp , -1 , sizeof(dp));
+    // }
+    // bool isSubsetSum(vector<int>a, int sum ,int i=0){
         
-        if(sum==0) return true;
-        if(sum < 0) return false;
-        if(i == a.size()) return false;
+    //     if(sum==0) return true;
+    //     if(sum < 0) return false;
+    //     if(i == a.size()) return false;
         
-        if(dp[i][sum] != -1) return dp[i][sum];
+    //     if(dp[i][sum] != -1) return dp[i][sum];
         
-        bool m = isSubsetSum(a, sum - a[i] , i+1);
-        bool n = isSubsetSum(a, sum , i+1);
+    //     bool m = isSubsetSum(a, sum - a[i] , i+1);
+    //     bool n = isSubsetSum(a, sum , i+1);
+    //     return dp[i][sum] = m||n;
+    // }
+    bool isSubsetSum(vector<int> a, int sum){
+        int dp[101][10001];
+        for(int i=0;i<=a.size();i++) dp[i][0] = 1;
+        for(int i=1;i<=sum;i++) dp[0][i] = 0;
         
-        return dp[i][sum] = m||n;
+        for(int i=1;i<=a.size();i++){
+            for(int j=1;j<=sum;j++){
+                if(j >= a[i-1]){
+                    dp[i][j] = dp[i-1][j - a[i-1]] || dp[i-1][j];
+                }
+                else dp[i][j] = dp[i-1][j];
+            }
+        }
+        
+        return dp[a.size()][sum];
     }
 };
 
