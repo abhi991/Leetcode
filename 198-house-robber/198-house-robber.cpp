8a9aa955be3da1dfd476f93d715c4c66ec7731f7
Abhijeet Srivastava <59133851+abhi991@users.noreply.github.com>
2022-06-16 21:37:50 +0530

    Time: 8 ms (6.07%), Space: 9.4 MB (6.32%) - LeetHub

---
+++ 198-house-robber/198-house-robber.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int solve(vector<int> a , int n , bool isAdjacent , vector<int> &dp){
+        if(n <= 0) return 0;
+        if(dp[n] != -1) return dp[n];
+        
+        return dp[n] = max((a[n-1] + solve(a , n-2 , true,dp)) ,solve(a , n-1 , false ,dp));
+    }
+    int rob(vector<int>&nums) {
+        vector<int> dp(nums.size()+1, -1);
+        int x = solve(nums, nums.size() , false , dp);
+        for(auto x:dp){
+            cout << x << " ";
+        }
+        return x;
+    }
+};
