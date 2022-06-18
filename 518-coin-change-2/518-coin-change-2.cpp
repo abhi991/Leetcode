32a7f9296b0cc16fcfe224fc535f23bf70987343
Abhijeet Srivastava <59133851+abhi991@users.noreply.github.com>
2022-06-18 16:33:52 +0530

    Time: 51 ms (36.40%), Space: 19.4 MB (12.75%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -9,6 +9,16 @@ public:
     int change(int amount, vector<int>& coins) {
         int dp[coins.size()+1][10001];
         memset(dp , -1 , sizeof(dp));
-        return helper(coins, amount,coins.size(),dp);
+        for(int i=0;i<=amount;i++) dp[0][i]=0;
+        for(int i=0;i<=coins.size();i++) dp[i][0]=1;
+        for(int i=1;i<=coins.size();i++){
+            for(int j=1;j<=amount;j++){
+                if(j >= coins[i-1]){
+                    dp[i][j] = dp[i][j-coins[i-1]] + dp[i-1][j];
+                }
+                else dp[i][j] = dp[i-1][j];
+            }
+        }
+        return dp[coins.size()][amount];
     }
 };
