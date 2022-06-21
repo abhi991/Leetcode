1061a21074418cb1b1ac103a8ef07d9c43102d07
Abhijeet Srivastava <59133851+abhi991@users.noreply.github.com>
2022-06-21 22:54:03 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (50.29%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,9 +1,23 @@
 class Solution {
 public:
-    vector<vector<int>> dp{101 , vector<int>(101,-1)};
-    int uniquePaths(int r, int c) {
-        if(r == 1 || c == 1) return 1;
-        if(dp[r][c] != -1) return dp[r][c];
-        return dp[r][c] = uniquePaths(r-1,c) + uniquePaths(r,c-1);
+    // int uniquePathsMEM(int r, int c) {
+    //     if(r == 1 || c == 1) return 1;
+    //     if(dp[r][c] != -1) return dp[r][c];
+    //     return dp[r][c] = uniquePaths(r-1,c) + uniquePaths(r,c-1);
+    // }
+    int uniquePaths(int r, int c){
+        vector<vector<int>> dp(r+1 , vector<int>(c+1,0));
+        for(int i=1;i<=r;i++){
+            dp[i][1] = 1;
+        }
+        for(int i=1;i<=c;i++){
+            dp[1][i] = 1;
+        }
+        for(int i=2;i<=r;i++){
+            for(int j=2;j<=c;j++){
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[r][c];
     }
 };
