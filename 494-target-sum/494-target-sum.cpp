9b99b8174e4ce2d946f2709f56c14c6ff3be04c6
Abhijeet Srivastava <59133851+abhi991@users.noreply.github.com>
2022-06-22 12:39:38 +0530

    Time: 14 ms (78.26%), Space: 9.1 MB (61.78%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -10,10 +10,24 @@ public:
         return dp[n][s+1000] = add+sub;
     
     }
-    int findTargetSumWays(vector<int>& nums, int target) {
-        int dp[101][2001];
-        memset(dp,-1,sizeof(dp));
+    int findTargetSumWays(vector<int>& nums, int s) {
         int n = nums.size();
-        return solve(nums,target,n,dp,0);
+        int sum=0;
+        for(auto x:nums) sum+=x;
+        if(s > sum || s < -sum) return 0;
+        int dp[n+1][sum * 2 + 1];
+        memset(dp,0,sizeof(dp));
+        dp[0][nums[0]+sum] = 1;
+        dp[0][-nums[0]+sum] += 1;
+        for (int i = 1; i < nums.size(); i++) {
+            for (int j = -sum; j <= sum; j++) {
+                if (dp[i-1][j+sum] != 0) {
+                    int tp = nums[i];
+                    dp[i][j+sum+tp] += dp[i-1][j+sum];
+                    dp[i][j+sum-tp] += dp[i-1][j+sum];
+                }
+            }
+        }
+        return dp[n-1][s+sum];
     }
 };
