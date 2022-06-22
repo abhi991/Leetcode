9158dd10f54999dc4873262cf5d14ef0867ea323
Abhijeet Srivastava <59133851+abhi991@users.noreply.github.com>
2022-06-22 14:00:00 +0530

    Time: 27 ms (55.01%), Space: 9 MB (78.73%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,33 +1,37 @@
-class Solution {
-public:
-    int solve(vector<int> &a , int t, int n , int dp[101][2001] , int s){
-        if(n == 0 && s == t) return 1;
-        if(n == 0) return 0;
-        if(dp[n][s+1000] != -1) return dp[n][s+1000];
-        int count=0;
-        count += solve(a,t,n-1,dp,s+a[n-1]);
-        count += solve(a,t,n-1,dp,s-a[n-1]);
-        return dp[n][s+1000] = count;
-    }
-    int findTargetSumWays(vector<int>& nums, int s) {
-        int n = nums.size();
-        int sum=0;
-        for(auto x:nums) sum+=x;
-        if(s > sum || s < -sum) return 0;
-        int dp[n+1][sum * 2 + 1];
-        memset(dp,0,sizeof(dp));
-        dp[0][nums[0]+sum] = 1;
-        dp[0][-nums[0]+sum] += 1;
-        for (int i = 1; i < nums.size(); i++) {
-            for (int j = -sum; j <= sum; j++) {
-                if (dp[i-1][j+sum] != 0) {
-                    int tp = nums[i];
-                    dp[i][j+sum+tp] += dp[i-1][j+sum];
-                    dp[i][j+sum-tp] += dp[i-1][j+sum];
+class Solution
+{
+    public:
+        int countSubsets(vector<int> &nums, int n, int M)
+        {
+            int t[n + 1][M + 1];
+            for (int i = 0; i <= n; i++){
+                for (int j = 0; j <= M; j++){
+                    if (i == 0)
+                        t[i][j] = 0;
+                    if (j == 0)
+                        t[i][j] = 1;
                 }
             }
+            for (int i = 1; i <= n; i++){
+                for (int j = 0; j <= M; j++){
+                    if (nums[i - 1] <= j)
+                        t[i][j] = t[i - 1][j - nums[i - 1]] + t[i - 1][j];
+                    else
+                        t[i][j] = t[i - 1][j];
+                }
+            }
+
+            return t[n][M];
         }
+    int findTargetSumWays(vector<int> &nums, int target){
+        target = abs(target);
+        int n = nums.size();
+        int sum = 0;
+        for (int i = 0; i < n; i++) sum += nums[i];
+        int M = (sum + target) / 2;
         
-        return dp[n-1][s+sum];
+        if (sum < target || (sum + target) % 2 != 0)
+            return 0;
+        return countSubsets(nums, n, M);
     }
 };
