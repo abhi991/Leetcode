7b44335fd93c2359dbf373a64b7e9b26f5ee943e
Abhijeet Srivastava <59133851+abhi991@users.noreply.github.com>
2022-06-22 11:32:11 +0530

    Time: 127 ms (52.25%), Space: 11.7 MB (73.75%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
-    int dp[10001];
-    int coinChang(vector<int>& coins, int s){
-        if(s == 0) return 0;
-        int ans = INT_MAX-1;
-        if(dp[s] != -1) return dp[s];
-        
-        for(int i=1;i<=coins.size();i++){
-            if(s >= coins[i-1]){
-                ans = min(ans, 1 + coinChang(coins, s - coins[i-1]));
-                dp[s] = ans;
-            }
+    int dp[20][10001];
+    int f(vector<int>&a, int n, int s){
+        if(s==0) return 0;
+        if(n == 0) return INT_MAX-1;
+        if(dp[n][s] != -1) return dp[n][s];
+        if(s >= a[n-1]){
+            return dp[n][s] = min(f(a,n-1,s), 1 + f(a,n,s-a[n-1]));
         }
-        
-        return (ans == INT_MAX ? INT_MAX-1 : ans);
+        return dp[n][s] = f(a, n-1, s);
     }
-    int coinChange(vector<int>& coins, int s) {
-        memset (dp , -1 , sizeof(dp));
-        int x = coinChang(coins,s);
-        return (x == INT_MAX-1 ? -1 : x);
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        memset(dp,-1,sizeof(dp));
+        int x = f(coins,n,amount);
+        return x == INT_MAX-1 ? -1 : x;
     }
 };
